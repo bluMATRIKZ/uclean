dee5619a438ca70d5d21618b4aadeab3e8a1f306
bluMATRIKZ <blumatrikz@gmail.com>
2025-06-22 09:47:14 -0700

    Add copyright notice

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+/*
+uclean - Removes temp files from removable media on linux.
+Copyright (C) 2025 Connor Thomson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or 
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include <stdlib.h>
